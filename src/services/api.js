f429d357d9ee7b1ae1144ac6ea589b2ef6209e66
Rafael Nascimento <rafaelradan76@gmail.com>
2021-10-03 09:36:49 -0300

    Trying to fix the baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API
+    /* baseURL: process.env.REACT_APP_API */
+    baseURL: 'https://first-api-rafael.herokuapp.com'
 })
 
 
